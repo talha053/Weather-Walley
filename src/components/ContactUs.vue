23573d7e0ab03914a5299cdb5309d8ccfb0198ae
talha053 <dev.talha053@gmail.com>
2024-07-10 14:34:44 +0500

    Add files via upload

---
+++ src/components/ContactUs.vue
@@ -0,0 +1,103 @@
+<template>
+
+  <div class="h-[100vh]">
+    <div class="w-full flex py-8">
+      <!--       Form -->
+      <div className="w-[50%] flex items-center justify-center text-center">
+
+        <div className="w-[70%]"><p className="text-4xl font-black text-[#98C3ED] font-serif  pb-4">Report Bug</p>
+
+          <form className=" mx-auto p-4 border rounded-lg shadow-lg px-12 py-12" @submit.prevent="handleSubmit">
+            <div className="mb-4">
+
+              <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  placeholder="Enter your name"
+                  className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                  required
+                  v-model="formData.name"
+              />
+            </div>
+            <div className="mb-4">
+
+              <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  v-model="formData.email"
+
+                  className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                  required
+              />
+            </div>
+            <div className="mb-4">
+
+              <input
+                  type="text"
+                  id="phone"
+                  name="phone"
+                  placeholder="Enter your phone number"
+                  v-model="formData.phone"
+
+                  className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                  required
+              />
+            </div>
+            <div className="mb-4">
+              <textarea v-model="formData.message" id="Report Bug" placeholder="Report Bug" required rows="4"
+                        className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"></textarea>
+
+
+            </div>
+            <button type="submit"
+                    className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 drop-shadow-lg active:bg-[#98c3ed]">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+      <!--        form end-->
+      <!--      image-->
+      <div class="w-1/2 h-[70vh] flex justify-center relative">
+        <img src="../assets/13429921_5255892.svg"
+             alt="Vue logo"
+             class="h-full w-full absolute">
+      </div>
+      <!--      image ends-->
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'ContactUs',
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        phone: '',
+        message: '',
+      },
+      bugReport: [],
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log('Form Data:', this.formData);
+      this.bugReport.unshift({...this.formData});
+      console.log(this.bugReport);
+      localStorage.setItem('Bugs Report', JSON.stringify(this.bugReport));
+      this.formData.name = "";
+      this.formData.email = "";
+      this.formData.phone = "";
+      this.formData.message = "";
+    }
+  }
+};
+</script>
+
---
+++ src/components/HomePage.vue
@@ -0,0 +1,173 @@
+<script>
+import MapShow from "./MapBox.vue"
+import axios from 'axios'
+
+export default {
+  name: 'HomeComponent',
+  components: {
+    MapShow
+  },
+  data() {
+    return {
+      apidata: [],
+      lon: 0,
+      lat: 0,
+      city: [],
+      latChanger: 0,
+      longChanger: 0,
+      Day: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+      testAry: []
+    }
+  },
+  methods: {
+    dayGetter(ind) {
+      const date = new Date();
+      const day = date.getDay();
+      let i = day + ind;
+
+      if (i > 6) {
+        i -= 7;
+        return this.Day[i];
+      } else {
+        return this.Day[i];
+      }
+    },
+    handleViewMoreClick() {
+      console.log("button pressed")
+      if (this.$refs.menuSectionRef) {
+        this.$refs.menuSectionRef.scrollIntoView({behavior: 'smooth'});
+      }
+
+    },
+    async getData() {
+      try {
+        const lond = this.$store.state.long
+        const latd = this.$store.state.lat
+        const apikey = import.meta.env.VITE_WEATHER_KEY
+        console.log(latd, lond)
+        const res = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?appid=${apikey}&lat=${latd}&lon=${lond}&cnt=7&units=metric&lang=en`);
+        if (res) {
+          this.apidata = res.data.list;
+          this.apidata = res.data.list;
+          this.city = res.data.city.name
+        }
+
+      } catch (e) {
+        console.log(e.message);
+      }
+    },
+
+  },
+  created() {
+    this.getData();
+  },
+  computed: {
+    laTo() {
+      return (this.latChanger = this.$store.state.lat)
+    }
+  },
+  watch: {
+    laTo() {
+      this.getData();
+    }
+  }
+};
+</script>
+
+<template>
+  <!--    first page section start-->
+  <div>
+    <div class=" w-[100%] h-[95vh] ">
+      <div class="w-[100%]  h-[70vh] p-2 flex justify-center relative"><img src="../assets/WeatherForcast.svg"
+                                                                            alt="Vue logo"
+                                                                            class="h-[100%] w-[100%] absolute">
+
+
+      </div>
+      <div class="flex w-full flex-col justify-center items-center text-center">
+        <h1 class="font-bold text-3xl pb-3">Welcome to Weather Walley</h1>
+
+        <button class="bg-[#79A3FF] py-2 px-8 rounded-lg shadow-lg font-bold " @click="handleViewMoreClick">Get Weekly
+          Updates
+        </button>
+      </div>
+    </div>
+    <div class="p-[3px] bg-[#ACD4F7]">
+
+    </div>
+    <!--    first page section end-->
+    <!--    map section start-->
+    <div ref="menuSectionRef">
+    <div class="flex w-[100%]" >
+      <div class="w-[50%]  h-[70vh] relative">
+
+        <MapShow/>
+
+
+      </div>
+
+
+      <div class="w-[50%] border h-[70vh] flex justify-center relative ">
+        <p class="text-3xl bold pt-2">
+          Weekly Updates
+        </p>
+        <img src="../assets/6982819_3353164.svg"
+             alt="Vue logo"
+             class="h-[100%] w-[100%] absolute">
+
+      </div>
+    </div>
+    </div>
+    <!--    map section end-->
+    <div class="p-[3px] bg-[#ACD4F7]">
+
+    </div>
+  </div>
+
+  <!--  Weather List Render start-->
+  <div class="max-w-[100%]  p-4 flex justify-center my-8">
+    <div class="border  rounded-lg shadow-lg w-[85%] p-8">
+
+      <div class="w-[100%]">
+        <div class=" flex justify-center text-3xl font-bold">Weekly Updates of
+          <!--          {{ this.city.length === 0 ? "Islamabad" : this.city }}-->
+          {{ this.city.length === 0 ? "Islamabad" : this.$store.state.city }}
+        </div>
+        <div class="py-4"><p class="text-2xl flex justify-center ">Stay One Step Ahead with Our Comprehensive Weather
+          Forecasts – Know What's Coming Your Way</p></div>
+      </div>
+      <ul>
+        <li v-for="(item, index) in apidata" :key="item.dt" class="my-4 ">
+          <div class="border-4 border-[#ACD4F7] rounded-lg shadow-lg py-5">
+            <div class="w-[100%] flex justify-center"><p class="font-bold text-2xl ">{{ dayGetter(index) }}</p></div>
+            <div class="flex w-[100%]">
+              <div class="w-[90%] pt-4">
+                <p class="font-[400] text-xl font-bold">Daily Report : </p>
+                <div class="flex justify-around py-4">
+                  <div>
+                    <p class="font-[400] text-xl">Weather: {{ item.weather[0].main }}</p>
+                    <p class="font-[400] pt-1 text-xl">Description: {{ item.weather[0].description }}</p>
+                  </div>
+                  <div class="">
+                    <p class="font-[400] text-xl">Humidity: {{ item.main.humidity }}</p>
+                    <p class="font-[400] pt-1 text-xl">Temp: {{ item.main.temp }} C</p>
+                  </div>
+                </div>
+
+
+              </div>
+              <div class="w-[10%]">
+                <img width="100%" height="100%" :src="`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`"
+                     alt="Weather Icon">
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+
+      <div>
+      </div>
+    </div>
+  </div>
+  <!--  Weather List Render end-->
+</template>
